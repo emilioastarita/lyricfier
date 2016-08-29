903126e6d65716bd81b9e2a16fc88b43d5cd81a9
Emilio Astarita <emilio.astarita@gmail.com>
2016-08-28 21:00:34 -0300

    Removed jquery dep.

--- render/render-typings.d.ts
+++ render/render-typings.d.ts
@@ -1,10 +1,5 @@
 /// <reference path="../node_modules/@types/electron/index.d.ts" />
-/// <reference path="../node_modules/@types/jquery/index.d.ts" />
 /// <reference path="../node_modules/@types/node/index.d.ts" />
 /// <reference path="../node_modules/@types/vue/index.d.ts" />
 
 declare let Notification : any;
-
-interface JQuery {
-    sideNav(command?: string): JQuery;
-}
