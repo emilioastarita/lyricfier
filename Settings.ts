78ef93705f547aefe150b07d702e84d1f7d7d68f
Emilio Astarita <emilio.astarita@gmail.com>
2016-12-04 11:41:16 -0300

    Settings working with default values.

--- Settings.ts
+++ Settings.ts
@@ -1,30 +1,40 @@
 import storage = require('electron-json-storage');
 
+
 interface SettingsValues {
     alwaysOnTop: boolean;
+    theme: 'dark'|'light';
 }
 
 
 export class Settings {
 
-    protected raw: SettingsValues;
+    protected raw: SettingsValues = <SettingsValues>{};
 
     getRaw() {
         return this.raw;
     }
 
+    protected setDefaults(settings: SettingsValues) {
+        settings.alwaysOnTop = false;
+        settings.theme = 'dark';
+    }
+
     load(ready) {
+        this.setDefaults(this.raw);
         storage.get('settings', (err, savedSettings: SettingsValues) => {
             if (err) savedSettings = <SettingsValues>{};
-            if (('alwaysOnTop' in savedSettings) === false) {
-                savedSettings['alwaysOnTop'] = this.raw.alwaysOnTop;
+            for (let attr in this.raw) {
+                if ((attr in savedSettings) === false) {
+                    savedSettings[attr] = this.raw[attr];
+                }
             }
             this.raw = savedSettings;
             ready();
         });
     }
 
-    save(newSettings, ready) {
+    save(newSettings, ready?) {
         let oldSettings = {};
         for (let attr in newSettings) {
             oldSettings[attr] = this.raw[attr];
@@ -38,13 +48,13 @@ export class Settings {
             }
             this.persist();
         }
-        ready();
+        ready && ready();
     }
 
     persist(ready?) {
         storage.set('settings', this.raw, (err) => {
             if (err) console.log('Err persisting settings', err);
-            ready(err);
+            ready && ready(err);
         });
     }
 
