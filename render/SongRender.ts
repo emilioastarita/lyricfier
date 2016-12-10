68bb422525563aae995ad1151796c818bd707595
Mark Mendoza <mendoza.mark.a@gmail.com>
2016-12-09 23:23:17 -0500

    Displaying data immediately upon reciept

--- render/SongRender.ts
+++ render/SongRender.ts
@@ -1,6 +1,7 @@
 import Component from 'vue-class-component';
 import {Searcher} from "./Searcher";
 import {template} from './template';
+import {SpotifyService} from './SpotifyService';
 
 @Component({
     props: {
@@ -15,6 +16,8 @@ import {template} from './template';
 })
 
 export class SongRender {
+    protected lastSongSync;
+    protected service:SpotifyService;
     protected song;
     protected shell;
     protected searcher: Searcher;
@@ -25,7 +28,8 @@ export class SongRender {
     data() {
         return {
             song: null,
-            searcher: new Searcher(this.notifyStatus),
+            lastSongSync: {},
+            searcher: new Searcher(),
             nextCallTime: 5000
         }
     }
@@ -49,15 +53,67 @@ export class SongRender {
     }
 
     refresh() {
-        this.searcher.syncLyrics((error, song, changed) => {
-            if (!error && changed) {
-                this.song = song;
-                this['$nextTick'](() => {
-                    document.getElementById("lyricBox").scrollTop = 0;
-                })
+        console.log('refreshing');
+        this.getSpotify().getCurrentSong((err, song) => {
+            if (err) {
+                this.notifyStatus('Current song error: ' + err);
+                this.scheduleNextCall();
+            } else if (this.isLastSong(song)) {
+                console.log('is last song nothing to do here');
+                this.scheduleNextCall();
+            } else {
+                console.log('is not last song searching by title and artist');
+                song.lyric = 'Loading Lyrics...';
+                this.displaySong(song);
+                this.saveLastSong(song);
+                this.searcher.search(song.title, song.artist, (err, lyric) => {
+                    if (err) {
+                        this.notifyStatus('Plugin error: ' + err);
+                        return;
+                    }
+                    if (lyric === null) {
+                      song.lyric = 'Sorry, couldn\'t find lyrics for this song!';
+                    } else {
+                      song.lyric = lyric;
+                    }
+                    this.displaySong(song);
+                    this['$nextTick'](() => {
+                        document.getElementById("lyricBox").scrollTop = 0;
+                    });
+                    this.scheduleNextCall();
+                });
             }
-            this.scheduleNextCall();
         });
+    }
+
+    displaySong(song) {
+      const newSongObject = {};
+      for (let k of Object.keys(song)) {
+          newSongObject[k] = song[k];
+      }
+      this.song = newSongObject;
+    }
+
+    isLastSong(song) {
+        for (let k of ['artist', 'title']) {
+            if (song[k] !== this.lastSongSync[k]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    saveLastSong(song) {
+        for (let k of Object.keys(song)) {
+            this.lastSongSync[k] = song[k];
+        }
+    }
+
+    getSpotify() {
+        if (!this.service) {
+            this.service = new SpotifyService();
+        }
+        return this.service;
     }
 
     openExternal(url) {
