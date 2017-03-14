a3a237775dd2341fadde1933ef877dd71850453b
Benjamin <benjamin.v.renterghem@gmail.com>
2017-03-14 22:54:36 +0100

    fixes development setup

--- render/render-typings.d.ts
+++ render/render-typings.d.ts
@@ -1,5 +1,4 @@
 /// <reference path="../node_modules/@types/electron/index.d.ts" />
 /// <reference path="../node_modules/@types/node/index.d.ts" />
-/// <reference path="../node_modules/@types/vue/index.d.ts" />
 
-declare let Notification : any;
+// declare let Notification : any;
